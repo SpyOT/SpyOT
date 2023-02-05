4b18588252f0870524283a152c5e547e07aa9807
Ken <kescovilla@gmail.com>
2023-02-05 13:30:52 -0800

    Initial Vue.JS w/ Routing Commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>Department of Computer Science & Engineering</h1>
+    <h2>University of Nevada, Reno</h2>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    /* display: flex; */
+    /* align-items: center; */
+  }
+}
+</style>
