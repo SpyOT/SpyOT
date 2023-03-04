564b924d8b5b7d1af232fdf15f669aaa81c7a9fb
Javier <japerez@nevada.unr.edu>
2023-03-04 05:29:18 -0800

    Added all components to main menu, still need to implement their command functionality and clean up style conflicts.

--- CS_425-26_Project-main/gui/app.py
+++ CS_425-26_Project-main/gui/app.py
@@ -1,4 +1,4 @@
-from tkinter import ttk, Frame, Canvas, Label, Button, PhotoImage
+from tkinter import Tk, ttk, Frame, Button, Label, NORMAL, PhotoImage
 from PIL import ImageTk
 import constants as preset
 
@@ -13,8 +13,8 @@ class App:
         self.main_menu = MainMenu(self.win)
 
     def configure_win(self):
-        self.win.resizable(False, False)
-        self.win.geometry(preset.default_geometry)
+        # self.win.resizable(False, False)
+        # self.win.geometry(preset.default_geometry)
         self.win.configure(bg="#202020")
         self.win.columnconfigure(0, weight=1)
         self.win.rowconfigure(0, weight=1)
@@ -46,12 +46,7 @@ class MainMenu:
         self.set_win()
 
         self.header_widgets = {}
-        self.buttons = {
-            "scan": None,
-            "collect": None,
-            "transfer": None,
-            "exit": None
-        }
+        self.body_widgets = {}
         self.set_widgets()
 
         self.display_win()
@@ -66,30 +61,34 @@ class MainMenu:
         self.header.columnconfigure(2, weight=1)
         self.header.rowconfigure(0, weight=1)
 
+        self.body.columnconfigure(0, weight=1)
+        self.body.rowconfigure(0, weight=1)
+        self.body.rowconfigure(1, weight=1)
+        self.body.rowconfigure(2, weight=1)
+        self.body.rowconfigure(3, weight=1)
+
     def handle_btn_press(self, option):
         match option:
             case "profile":
                 print("clicked on profile")
+            case "settings":
+                pass
+            case "scan":
+                pass
+            case "collect":
+                pass
+            case "upload":
+                pass
+            case "exit":
+                pass
 
     def set_widgets(self):
         # Header widgets
-        self.header_widgets["canvas"] = Canvas(
-            self.header,
-            bg="yellow",
-            bd=0,
-            highlightthickness=0,
-            relief="ridge"
-        )
-        canvas = self.header_widgets["canvas"]
-        profile_icon = ImageTk.PhotoImage(file=preset.profile_path)
+        profile_icon = PhotoImage(file=preset.profile_path)
         self.header_widgets["profile"] = Button(
             self.header,
             image=profile_icon,
-            borderwidth=0,
-            highlightthickness=0,
-            activebackground="#202020",
-            command=lambda: self.handle_btn_press("profile"),
-            relief="flat",
+            command=lambda: self.handle_btn_press("profile")
         )
         self.header_widgets["profile"].image = profile_icon
 
@@ -100,13 +99,55 @@ class MainMenu:
             activebackground="#202020")
         self.header_widgets["title"].image = title_icon
 
+        settings_icon = PhotoImage(file=preset.setting_path)
+        self.header_widgets["settings"] = Button(
+            self.header,
+            image=settings_icon
+        )
+        self.header_widgets["settings"].image = settings_icon
+
+        # Body widgets
+        self.body_widgets["scan"] = Button(
+            self.body,
+            text="Scan Network",
+            command=lambda: self.handle_btn_press("scan")
+        )
+        self.body_widgets["collect"] = Button(
+            self.body,
+            text="Collect Data",
+            command=lambda: self.handle_btn_press("collect")
+        )
+        self.body_widgets["upload"] = Button(
+            self.body,
+            text="Upload Data",
+            command=lambda: self.handle_btn_press("upload")
+        )
+        self.body_widgets["exit"] = Button(
+            self.body,
+            text="Exit Program",
+            command=lambda: self.handle_btn_press("exit")
+        )
+
     def display_win(self):
         self.container.grid(column=0, row=0, sticky='nesw')
         self.header.grid(column=0, row=0, sticky='nesw', padx=5, pady=5)
         self.body.grid(column=0, row=1, sticky='nesw', padx=5, pady=5)
 
-        # self.header_widgets["canvas"].place(x=0, y=0)
-        # btn = self.header_widgets["profile"]
-        # self.header_widgets["canvas"].create_window(0, 0, window=btn)
-        self.header_widgets["profile"].grid(column=0, row=0)
-        self.header_widgets["title"].grid(column=1, row=0)
+        for i, option in enumerate(self.header_widgets):
+            self.header_widgets[option].grid(
+                column=i,
+                row=0,
+                padx=15, pady=15)
+        # self.header_widgets["profile"].grid(column=0, row=0)
+        # self.header_widgets["title"].grid(column=1, row=0)
+        # self.header_widgets["settings"].grid(column=2, row=0)
+
+        for i, option in enumerate(self.body_widgets):
+            self.body_widgets[option].grid(
+                column=0,
+                row=i,
+                padx=15, pady=15)
+        # self.body_widgets["scan"].grid(column=0, row=0, padx=15, pady=15)
+        # self.body_widgets["collect"].grid(column=0, row=1)
+        # self.body_widgets["upload"].grid(column=0, row=2)
+        # self.body_widgets["exit"].grid(column=0, row=3)
