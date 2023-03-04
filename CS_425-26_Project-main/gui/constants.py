e121aa991483ab48a0b42dbf0b26ec96f6146a48
Javier <japerez@nevada.unr.edu>
2023-03-03 22:19:58 -0800

    Asset name updates and paths added to constants

--- CS_425-26_Project-main/gui/constants.py
+++ CS_425-26_Project-main/gui/constants.py
@@ -7,3 +7,6 @@ logo_img = ""
 default_text_size = 18
 default_font = "Helvetica"
 padding = (3, 3, 12, 12)
+profile_path = "assets/profile_icon.png"
+setting_path = "assets/settings_icon.png"
+title_path = "assets/title_icon.png"
